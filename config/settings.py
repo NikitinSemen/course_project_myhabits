26f81df9802e5bdcc70d9e2319acafe0760133d8
webcam <pr.pypblrka@gmail.com>
2024-09-05 12:03:51 +0400

    git-ci.yaml

--- config/settings.py
+++ config/settings.py
@@ -74,11 +74,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT"),
+        "NAME": "my_habits",
+        "USER": "postgres",
+        # "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        # "HOST": os.getenv("POSTGRES_HOST"),
+        # "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
